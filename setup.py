8b45435144956a956d705393a9901b0f30dd80e7
joshwills <joshewills@gmail.com>
2019-03-21 12:54:22 +0000

    Version increment.

--- setup.py
+++ setup.py
@@ -14,12 +14,12 @@ plugin_package = "octoprint_mgsetup"
 plugin_name = "OctoPrint-Mgsetup"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.3.0.9.9"
+plugin_version = "0.3.1.0.0"
 
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
-plugin_description = """TODO"""
+plugin_description = """MakerGear Setup plugin for the U1"""
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
 plugin_author = "MGDevTeam"
